3d6e6185ba207d1637e3d7b7fc4f2f19aba87be3
sampaw13 <samarielpaw@gmail.com>
2018-07-27 15:03:04 +0000

    automatically backed up by learn

---
+++ book.rb
@@ -0,0 +1,22 @@
+class Car 
+  def initialize(model, make, year)
+    @carmodel = model
+    @carmake = make
+    @birthyear = year 
+    
+    def model 
+      @carmodel
+    end 
+    
+    def make 
+      @carmake
+    end 
+    
+    def year 
+      @birthyear
+    end 
+    
+    #model make and year are the attributes 
+    
+    
+    
